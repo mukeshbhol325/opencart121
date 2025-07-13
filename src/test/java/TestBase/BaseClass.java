3b123908ea7afe8f2e19220b4e472ab82a2dcacd
Mukesh <mukeshbhol325@gmail.com>
2025-07-14 00:16:45 +0530

    Added Functionality for Remote executions

--- src/test/java/TestBase/BaseClass.java
+++ src/test/java/TestBase/BaseClass.java
@@ -3,6 +3,7 @@ package TestBase;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -12,10 +13,13 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -36,10 +40,36 @@ public class BaseClass {
 		
 		logger=LogManager.getLogger(this.getClass());
 		
+		if(p.getProperty("execution").equalsIgnoreCase("remote")) {
+			DesiredCapabilities capabilities=new DesiredCapabilities();
+			
+			//OS
+			if(os.equalsIgnoreCase("windows")) {
+				capabilities.setPlatform(Platform.WIN11);
+			}
+			else if(os.equalsIgnoreCase("mac")) {
+				capabilities.setPlatform(Platform.MAC);
+			}
+			else {
+				System.out.println("No Matching os");
+				return;
+			}
+			//browser
+			switch(br.toLowerCase()) {
+			case "chrome":capabilities.setBrowserName("chrome");break;
+			case "edge":capabilities.setBrowserName("MicrosoftEdge");break;
+			default:System.out.println("No Matching Browser"); return;
+			}
+			
+			driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),capabilities);
+		}
+		else if(p.getProperty("execution").equalsIgnoreCase("local")) {
+		
 		switch(br.toLowerCase()) {
 		case "chrome": driver=new ChromeDriver();break;
 		case "edge": driver=new EdgeDriver();break;
 		default:System.out.println("Invalid Parameter");return;
+		}
 		}
 		
 		driver.manage().deleteAllCookies();
